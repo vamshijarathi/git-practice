3d3509251e38ed8c5a12926ef80ee0f319a3f491
VAMSHIKRISHNA JARATHI <vamshij888@gmail.com>
2026-02-13 23:28:46 +0530

    added multiplication function

---
+++ sample.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+    return a+b
+def substraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+print(addition(10,20))
+print(substraction(20,15))
+print(multiplication(30,40))
